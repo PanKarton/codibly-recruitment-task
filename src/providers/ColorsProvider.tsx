860c7ad505258d3c6c44518f5c8c25944e01dd2b
unknown <jack-swad@wp.pl>
2023-02-05 23:23:53 +0100

    added useeffect to colorsprovider

--- src/providers/ColorsProvider.tsx
+++ src/providers/ColorsProvider.tsx
@@ -1,23 +1,54 @@
-import React, { createContext, ReactNode, useContext } from 'react';
-import { useLoaderData } from 'react-router';
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { ColorsResponse } from 'types/colors-array-response';
+import { useParams } from 'react-router-dom';
 
 type Props = {
   children: ReactNode;
 };
 
-type LoaderData = {
-  colorsData: ColorsResponse;
+type Context = {
+  colorsData: ColorsResponse | null;
 };
 
-type Context = {
-  colorsData: ColorsResponse;
-};
+const pageSize = 5;
 
 const ColorsDataContext = createContext<Context | null>(null);
 
+const initColorsState = {
+  page: 1,
+  per_page: 6,
+  total: 12,
+  total_pages: 2,
+  data: [],
+  support: {
+    url: 'https://reqres.in/#support-heading',
+    text: 'To keep ReqRes free, contributions towards server costs are appreciated!',
+  },
+};
+
 export const ColorsDataProvider = ({ children }: Props) => {
-  const { colorsData } = useLoaderData() as LoaderData;
+  const [colorsData, setColorsData] = useState<ColorsResponse | null>(initColorsState);
+  const { pageIndex } = useParams();
+
+  useEffect(() => {
+    const fetchColors = async () => {
+      try {
+        if (!pageIndex) return setColorsData(null);
+
+        const response = await fetch(
+          `https://reqres.in/api/products?page=${pageIndex}&per_page=${pageSize}`
+        );
+
+        const colorsData = await response.json();
+
+        setColorsData(colorsData);
+      } catch (err) {
+        setColorsData(null);
+      }
+    };
+
+    fetchColors();
+  }, [pageIndex]);
 
   const contextData = {
     colorsData,
